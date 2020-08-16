d48ed80731385ce2664ee215b0b69a15566215b4
km-tr <kumagai@bear.tokyo>
2020-08-16 11:53:33 +0900

    update

---
+++ target/debug/about.d
@@ -0,0 +1,1 @@
+/Users/bearkm/hoge/Rustlings/target/debug/about: /Users/bearkm/hoge/Rustlings/Introduction/Getting\ started/About/src/main.rs
---
+++ target/debug/deps/about-456970d433ad0830.d
@@ -0,0 +1,5 @@
+/Users/bearkm/hoge/Rustlings/target/debug/deps/about-456970d433ad0830: Introduction/Getting\ started/About/src/main.rs
+
+/Users/bearkm/hoge/Rustlings/target/debug/deps/about-456970d433ad0830.d: Introduction/Getting\ started/About/src/main.rs
+
+Introduction/Getting\ started/About/src/main.rs:
